5e64bed66240376084b3a49502319f3110914954
ray <togneriraony@gmail.com>
2025-05-25 13:07:10 -0300

    ajustes no condicional

--- 01_eliminacao_gauss/eliminacao_gauss_com_pivotacao.m
+++ 01_eliminacao_gauss/eliminacao_gauss_com_pivotacao.m
@@ -1,8 +1,8 @@
 function[A, b, info] = eliminacao_gauss_com_pivotacao(n, A, b)
-  det = 1; info = 0
+  det = 1; info = 0;
   # processo pra escolher o pivot sendo o maior em valor abs da coluna
   for j=1:n-1
-    pivot = j; Amax = abs(A(j,j))
+    pivot = j; Amax = abs(A(j,j));
     for k=j+1:n
       if abs(A(k,j)) > Amax
         Amax = abs(A(k, j));    pivot = k;
@@ -10,28 +10,27 @@ function[A, b, info] = eliminacao_gauss_com_pivotacao(n, A, b)
     endfor
     if pivot != j
        for k=1:n
-         t = A(j, k);    A(j, k) = A(pivot, k);     A(pivot, k) = t
+         t = A(j, k);    A(j, k) = A(pivot, k);     A(pivot, k) = t;
        endfor
-       t = b(j);        b(j) = b(pivot);          b(pivot) = t;       det = -det
+       t = b(j);        b(j) = b(pivot);          b(pivot) = t;       det = -det;
     endif
-    det = det * A(j,j)
+    det = det * A(j,j);
     
     # processo de eliminacao de gauss
     if abs(A(j,j)) != 0
-      r = 1/A(j,j)
+      r = 1/A(j,j);
       for i=j+1:n
-        mult = A(i,j)*r;  A(i, j) = 0
+        mult = A(i,j)*r;  A(i, j) = 0;
         for k=j+1:n
-          A(i, k) = A(i, k) - mult*A(j, k)
+          A(i, k) = A(i, k) - mult*A(j, k);
         endfor
-        b(i) = b(i) - mult * b(j)
+        b(i) = b(i) - mult * b(j);
       endfor
-    endif
-    if info == 0
-      info = j
+    elseif info == 0
+      info = j;
     endif
   endfor
-  det = det * A(n, n)
+  det = det * A(n, n);
   if info == 0 && abs(A(n, n)) == 0
-    info = n
+    info = n;
   endif
--- 01_eliminacao_gauss/eliminacao_gauss_sem_pivotacao.m
+++ 01_eliminacao_gauss/eliminacao_gauss_sem_pivotacao.m
@@ -1,24 +1,23 @@
 function[A, b, info] = eliminacao_gauss_com_pivotacao(n, A, b)
-  det = 1; info = 0
+  det = 1; info = 0;
   # processo pra escolher o pivot sendo o maior em valor abs da coluna
   for j=1:n-1
     
     # processo de eliminacao de gauss
     if abs(A(j,j)) != 0
-      r = 1/A(j,j)
+      r = 1/A(j,j);
       for i=j+1:n
-        mult = A(i,j)*r;  A(i, j) = 0
+        mult = A(i,j)*r;  A(i, j) = 0;
         for k=j+1:n
-          A(i, k) = A(i, k) - mult*A(j, k)
+          A(i, k) = A(i, k) - mult*A(j, k);
         endfor
-        b(i) = b(i) - mult * b(j)
+        b(i) = b(i) - mult * b(j);
       endfor
-    endif
-    if info == 0
-      info = j
+    elseif info == 0
+      info = j;
     endif
   endfor
-  det = det * A(n, n)
+  det = det * A(n, n);
   if info == 0 && abs(A(n, n)) == 0
-    info = n
+    info = n;
   endif
